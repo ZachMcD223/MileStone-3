37a8b11665e268534e4adcc2751b952df18c0c4c
ZachMcD223 <148635518+ZachMcD223@users.noreply.github.com>
2024-07-25 20:43:00 -0500

    added .sequelizerc

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 //Dependencies
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize');
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
 
+// CONFIGURATION / MIDDLEWARE
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
 
+// LISTEN
+app.listen(4005, () => {
+    console.log('Server is running on port 4005');
+})
 
 // SEQUELIZE CONNECTION
-const sequelize = new Sequelize(process.env.PG_URI)
+// const sequelize = new Sequelize(process.env.PG_URI)
 
-try {
-    sequelize.authenticate() 
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`) 
-}
+// try {
+//     sequelize.authenticate() 
+//     console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+// } catch(err) {
+//     console.log(`Unable to connect to PG: ${err}`) 
+// }
