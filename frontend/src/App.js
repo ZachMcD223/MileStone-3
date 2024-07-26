999901df2f913886bf7d253c85f518fae21c8c3d
Dylan Ennis <dennis@ucsd.edu>
2024-07-26 05:13:31 -0700

    styled the home page with animations on scroll for pictures  and boxes with shadow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,35 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import Menu from './components/Menu';
-import SignUpForm from './components/users/SignUpForm';
-import LoginForm from './components/users/LoginForm';
-import Checkout from './components/Checkout';
-import Header from './components/Header'
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Menu from "./components/Menu";
+import SignUpForm from "./components/users/SignUpForm";
+import LoginForm from "./components/users/LoginForm";
+import Checkout from "./components/Checkout";
+import Header from "./components/Header";
+import "aos/dist/aos.css";
+import "./App.css";
+import AOS from "aos";
 
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
     <div className="App">
       <Router>
-      <header>
-      <Header/>
-      </header>
-      <main>
-      <Routes>
-         <Route path="/" element={<Home />} />
-         <Route path="/menu" element={<Menu />} />
-         <Route path="/sign-up" element={<SignUpForm />} />
-         <Route path="/sign-in" element={<LoginForm />} />
-         <Route path="/Checkout" element={<Checkout />} />
-      </Routes>
-      </main>
+        <header>
+          <Header />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/sign-up" element={<SignUpForm />} />
+            <Route path="/sign-in" element={<LoginForm />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </main>
       </Router>
     </div>
   );
