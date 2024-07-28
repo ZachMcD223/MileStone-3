16373dce1c39960c937bd5b7c900e8b72966efaf
Dylan Ennis <dennis@ucsd.edu>
2024-07-28 12:04:34 -0700

    fixing post stuff for auth

--- backend/controllers/customers.js
+++ backend/controllers/customers.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
-// const { Customer } = db
+const { Customers } = db
 const { Op } = require("sequelize");
-const Customer = require('../models/customers')
+// const Customers = require('../models/customers')
 
 
 // router.get('/', async (req, res) => {
-//     const customers = await Customer.findAll()
+//     const customers = await Customers.findAll()
 //     res.json(customers)
 // })
 
@@ -17,7 +17,7 @@ const Customer = require('../models/customers')
 // Get all customers
 router.get('/', async (req, res) => {
   try {
-    const customers = await Customer.find();
+    const customers = await Customers.find();
     res.json(customers);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -27,9 +27,9 @@ router.get('/', async (req, res) => {
 // Get a single customer by ID
 router.get('/:id', async (req, res) => {
   try {
-    const customer = await Customer.findById(req.params.id);
+    const customer = await Customers.findById(req.params.id);
     if (customer == null) {
-      return res.status(404).json({ message: 'Customer not found' });
+      return res.status(404).json({ message: 'Customers not found' });
     }
     res.json(customer);
   } catch (err) {
@@ -39,7 +39,7 @@ router.get('/:id', async (req, res) => {
 
 // Create a new customer
 // router.post('/', async (req, res) => {
-//   const customer = new Customer({
+//   const customer = new Customers({
 //     first_name: req.body.name,
 //     last_name: req.body.name,
 //     email: req.body.email,
@@ -48,8 +48,8 @@ router.get('/:id', async (req, res) => {
 //   });
 
 //   try {
-//     const newCustomer = await customer.save();
-//     res.status(201).json(newCustomer);
+//     const newCustomers = await customer.save();
+//     res.status(201).json(newCustomers);
 //   } catch (err) {
 //     res.status(400).json({ message: err.message });
 //   }
@@ -57,7 +57,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
     let { password, ...rest } = req.body;
-    const customer = await Customer.create({
+    const customer = await Customers.create({
         ...rest,
         role: 'reviewer',
         passwordDigest: await bcrypt.hash(password, 12)
@@ -68,9 +68,9 @@ router.post('/', async (req, res) => {
 // Update a customer
 router.put('/', async (req, res) => {
   try {
-    const customer = await Customer.findById(req.params.id);
+    const customer = await Customers.findById(req.params.id);
     if (customer == null) {
-      return res.status(404).json({ message: 'Customer not found' });
+      return res.status(404).json({ message: 'Customers not found' });
     }
 
     if (req.body.name != null) {
@@ -86,8 +86,8 @@ router.put('/', async (req, res) => {
       customer.address = req.body.address;
     }
 
-    const updatedCustomer = await customer.save();
-    res.json(updatedCustomer);
+    const updatedCustomers = await customer.save();
+    res.json(updatedCustomers);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
@@ -96,13 +96,13 @@ router.put('/', async (req, res) => {
 // Delete a customer
 router.delete('/', async (req, res) => {
   try {
-    const customer = await Customer.findById(req.params.id);
+    const customer = await Customers.findById(req.params.id);
     if (customer == null) {
-      return res.status(404).json({ message: 'Customer not found' });
+      return res.status(404).json({ message: 'Customers not found' });
     }
 
     await customer.remove();
-    res.json({ message: 'Customer deleted' });
+    res.json({ message: 'Customers deleted' });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
