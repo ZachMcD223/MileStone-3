81d35e425d46e1ab15cd0d0ade66e8ff35433c10
ZachMcD223 <148635518+ZachMcD223@users.noreply.github.com>
2024-07-30 01:16:38 -0500

    experimenting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 // with some help from https://www.youtube.com/watch?v=78YM00SO6uk
 import React from "react";
 import Logo from "./Logo";
-import Nav from "./Nav";
+
 
 export const Header = () => {
   return (
     <header className="bg-DarkRed sticky top-0 flex-wrap z-[20] mx-auto flex w-full items-center justify-between border-b border-gray-500 p-6">
       <Logo />
-      <Nav />
+ 
     </header>
   );
 };
